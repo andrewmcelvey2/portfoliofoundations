4ac31230ac52046fc9c09642a003db45c0e5a787
Andrew McElvey <andrew.mcelvey@gmail.com>
2025-11-07 23:39:11 -0600

    Add files via upload

--- contactMe/scripts/script.js
+++ contactMe/scripts/script.js
@@ -23,11 +23,44 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
         .map(cb => cb.value)
         .join(", ") || "None";
 
-    // Display everything in an alert
-    alert(
-        `--- CONTACT FORM DATA ---\n\n` +
-        `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
+    //Creating an arrray of Key-value pairs
+    const formData= [
+        ["Name", name], 
+        ["Email", email], 
+        ["Password", password], 
+        ["Phone", phone], 
+        ["Website", website], 
+        ["Age", age], 
+        ["Date of Birth", dob], 
+        ["Preferred Time", time], 
+        ["Favorite Color", color], 
+        ["Interest Level", range], 
+        ["Contact Method", contactMethod], 
+        ["Services Interested In", services], 
+        ["Topic", topic], 
+        ["Message", message], 
+    ];
+
+    //Showing the results section
+    const resultContainer = 
+    document.getElementById("resultContainer");
+    const resultBody = document.querySelector("#resultTable tbody");
+    // resultBody.innerHTML = ""; //Clear Previous entries
+    resultContainer.style.display = "block"; //Displaying the table
+
+    //Populate the table
+    formData.forEach(([key, value]) => {
+        const row = document.createElement("tr"); //Creates new rows
+        row.innerHTML = `<td>${key}</td><td>${value}</td>`;
+        resultBody.appendChild(row); //Stacks rows until finished
+    }
     );
+
+    // Display everything in an alert
+    // alert(
+    //     `--- CONTACT FORM DATA ---\n\n` +
+    //     `Name: ${name}\nEmail: ${email}\nPassword: ${password}\nPhone: ${phone}\nWebsite: ${website}\nAge: ${age}\nDate of Birth: ${dob}\nPreferred Time: ${time}\nFavorite Color: ${color}\nInterest Level: ${range}\nContact Method: ${contactMethod}\nServices Interested In: ${services}\nTopic: ${topic}\nMessage: ${message}`
+    // );
 
     // Reset the form
     this.reset();
